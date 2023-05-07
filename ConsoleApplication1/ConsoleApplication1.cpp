ad0ef48816edb0a0edfa67c48d609a6ae24ec154
nikit <nikit@DESKTOP-5363RTP>
2023-05-07 17:14:57 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,54 @@
+﻿# mainwindow.h
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow* ui;
+    QString filePath;
+};
+
+#endif // MAINWINDOW_H
+
+
+# mainwindow.cpp
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget* parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    filePath = "../../test141/config.ini";
+    QFile file(filePath);
+    file.open(QFile::ReadOnly);
+    while (!file.atEnd()) {
+        qDebug() << file.readLine();
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+# config.ini
+windowwidth = 1100
+windowheight = 900
